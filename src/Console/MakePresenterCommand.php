8b8e43fae4421fcdc35abe89d6b913a0789b2876
Oussama <imo.ossama1@gmail.com>
2022-08-14 21:48:46 +0100

    Update MakePresenterCommand.php

--- src/Console/MakePresenterCommand.php
+++ src/Console/MakePresenterCommand.php
@@ -4,7 +4,7 @@ namespace Coderflex\LaravelPresenter\Console;
 
 use Illuminate\Console\GeneratorCommand;
 
-class PresenterMakeCommand extends GeneratorCommand
+class MakePresenterCommand extends GeneratorCommand
 {
     public $name = 'make:presenter';
 
